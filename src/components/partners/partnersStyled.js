2b9f182e80360925e5cb22fb8b1b63e5e0139041
Sarahshr <saraah.sahraoui@gmail.com>
2020-12-17 20:04:49 +0100

    Add partners 's component

---
+++ src/components/partners/partnersStyled.js
@@ -0,0 +1,64 @@
+import styled from "styled-components";
+
+export const Main = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Title = styled.h2`
+  font-size: 30pt;
+  color: #001027;
+`;
+
+export const CategoryTitle = styled.span`
+  font-size: 18pt;
+  color: #001027;
+  margin-top: 3%;
+  margin-bottom: 2%;
+`;
+
+export const Bloc1 = styled.div`
+  display: flex;
+  justify-content: space-around;
+  width: 80%;
+  align-items: center;
+`;
+
+export const Bloc2 = styled.div`
+  display: flex;
+  justify-content: space-around;
+  width: 50%;
+  align-items: center;
+`;
+
+export const Blocs = styled.div`
+  display: flex;
+  align-items: center;
+  width: 60%;
+  justify-content: space-around;
+  //border: 1px solid red;
+`;
+
+export const BlocService = styled.div`
+  display: flex;
+  align-items: center;
+  width: 70%;
+  justify-content: space-between;
+  //border: 1px solid red;
+`;
+
+export const BlocAgro = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+`;
+
+export const Logo = styled.img`
+  width: 20%;
+`;
+
+export const LogoP = styled.img`
+  width: 20%;
+`;
