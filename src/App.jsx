4df7639cf0bd73d7a27ae032c5de7eb4f306911a
Sarahshr <saraah.sahraoui@gmail.com>
2020-12-23 23:32:01 +0100

    Import Timeline component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Timeline from "./Components/Timeline/Timeline";
+
 export default function App() {
   return (
-    <div>
-      <h1>Hello World !</h1>
-    </div>
+    <>
+      <Timeline />
+    </>
   );
 }
