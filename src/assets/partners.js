45c9ff2e6a3ec1678a9fa8593d3429979a133f20
ThomasMlh <storm62320@gmail.com>
2020-12-24 20:36:18 +0100

    Create table with partners logos

---
+++ src/assets/partners.js
@@ -0,0 +1,78 @@
+import quaternaire from "../assets/images/partners/quaternaire.png";
+import schneider from "../assets/images/partners/schneider-consulting.png";
+import dupont from "../assets/images/partners/dupont.png";
+import imma from "../assets/images/partners/imma.jpg";
+import xpm from "../assets/images/partners/x-pm.png";
+import sophartex from "../assets/images/partners/sophartex.jpeg";
+import unither from "../assets/images/partners/unither.png";
+import legendre from "../assets/images/partners/legendre.png";
+import alimakek from "../assets/images/partners/alimakek.png";
+import locapal from "../assets/images/partners/locapal.gif";
+import elivia from "../assets/images/partners/elivia.png";
+import dekra from "../assets/images/partners/dekra.png";
+import nes from "../assets/images/partners/nes.png";
+import cif from "../assets/images/partners/cif.png";
+import lagglo from "../assets/images/partners/lagglo.jpeg";
+import csgroup from "../assets/images/partners/cs-group.png";
+import cacom from "../assets/images/partners/cacom.png";
+import elvia from "../assets/images/partners/elvia.bmp";
+import peg from "../assets/images/partners/peg.jpeg";
+import gaultfremont from "../assets/images/partners/gault-fremont.png";
+import clariant from "../assets/images/partners/clariant.png";
+
+export const myPartners = [
+  {
+    name: "MainLogo",
+    logos: [
+      { src: quaternaire, alt: "quaternaire" },
+      { src: dupont, alt: "dupont" },
+      { src: schneider, alt: "schneider" },
+      { src: imma, alt: "imma" },
+      { src: xpm, alt: "imma" },
+    ],
+  },
+  {
+    name: "Pharmacie",
+    logos: [
+      { src: sophartex, alt: "sophartex" },
+      { src: unither, alt: "unither" },
+    ],
+  },
+  {
+    name: "Construction",
+    logos: [
+      { src: legendre, alt: "legendre" },
+      { src: alimakek, alt: "alimakek" },
+      { src: locapal, alt: "locapal" },
+    ],
+  },
+  {
+    name: "IT",
+    logos: [
+      { src: csgroup, alt: "csgroup" },
+      { src: cacom, alt: "cacom" },
+    ],
+  },
+  {
+    name: "Services",
+    logos: [
+      { src: dekra, alt: "dekra" },
+      { src: nes, alt: "nes" },
+      { src: cif, alt: "cif" },
+      { src: lagglo, alt: "lagglo" },
+    ],
+  },
+  {
+    name: "Industries",
+    logos: [
+      { src: elvia, alt: "elvia" },
+      { src: peg, alt: "peg" },
+      { src: gaultfremont, alt: "gaultfremont" },
+      { src: clariant, alt: "clariant" },
+    ],
+  },
+  {
+    name: "Agro-alimentaire",
+    logos: [{ src: elivia, alt: "elivia" }],
+  },
+];
