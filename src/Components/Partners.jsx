3a9e1364f950d648ddc13f3214f85839907cbe37
ThomasMlh <storm62320@gmail.com>
2020-12-24 20:35:20 +0100

    Delete import, rework code

--- src/Components/Partners.jsx
+++ src/Components/Partners.jsx
@@ -1,35 +1,8 @@
 import React from "react";
+import { myPartners } from "../assets/partners";
 
-import quaternaire from "../assets/images/partners/quaternaire.png";
-import schneider from "../assets/images/partners/schneider-consulting.png";
-import dupont from "../assets/images/partners/dupont.png";
-import imma from "../assets/images/partners/imma.jpg";
-import xpm from "../assets/images/partners/x-pm.png";
-import sophartex from "../assets/images/partners/sophartex.jpeg";
-import unither from "../assets/images/partners/unither.png";
-import legendre from "../assets/images/partners/legendre.png";
-import alimakek from "../assets/images/partners/alimakek.png";
-import locapal from "../assets/images/partners/locapal.gif";
-import elivia from "../assets/images/partners/elivia.png";
-import dekra from "../assets/images/partners/dekra.png";
-import nes from "../assets/images/partners/nes.png";
-import cif from "../assets/images/partners/cif.png";
-import lagglo from "../assets/images/partners/lagglo.jpeg";
-import csgroup from "../assets/images/partners/cs-group.png";
-import cacom from "../assets/images/partners/cacom.png";
-import elvia from "../assets/images/partners/elvia.bmp";
-import peg from "../assets/images/partners/peg.jpeg";
-import gaultfremont from "../assets/images/partners/gault-fremont.png";
-import clariant from "../assets/images/partners/clariant.png";
 import {
   Bloc1,
-  Bloc2,
-  BlocSize60,
-  BlocAgro,
-  BlocSize70,
-  Logo,
-  LogoXpm,
-  Logo20,
   Logo15,
   Main,
   Title,
@@ -37,58 +10,38 @@ import {
 } from "../styled-components/Partners";
 
 const Partners = () => {
+  console.log(myPartners.filter((part) => part.name === "MainLogo"));
   return (
     <Main>
       <Title>Nos partenaires et nos clients</Title>
-      <Bloc1>
-        <Logo src={quaternaire} />
-        <Logo src={dupont} />
-        <Logo src={schneider} />
-      </Bloc1>
-      <Bloc2>
-        <Logo src={imma} />
-        <LogoXpm src={xpm} />
-      </Bloc2>
+      {myPartners
+        .filter((part) => part.name === "MainLogo")
+        .map((partner) => {
+          return (
+            <Bloc1>
+              {partner.logos.map((logo) => {
+                return <Logo15 src={logo.src} alt={logo.alt} />;
+              })}
+            </Bloc1>
+          );
+        })}
+      {myPartners
+        .filter((part) => part.name != "MainLogo")
+        .map((partner) => {
+          return (
+            <Bloc1>
+              {partner.name ? (
+                <CategoryTitle>{partner.name}</CategoryTitle>
+              ) : (
+                ""
+              )}
 
-      <CategoryTitle>Pharmacie</CategoryTitle>
-      <BlocSize60>
-        <Logo20 src={sophartex} />
-        <Logo20 src={unither} />
-      </BlocSize60>
-
-      <CategoryTitle>Construction</CategoryTitle>
-      <BlocSize70>
-        <Logo20 src={legendre} />
-        <Logo20 src={alimakek} />
-        <Logo20 src={locapal} />
-      </BlocSize70>
-
-      <CategoryTitle>IT</CategoryTitle>
-      <BlocSize60>
-        <Logo20 src={csgroup} />
-        <Logo20 src={cacom} />
-      </BlocSize60>
-
-      <CategoryTitle>Services</CategoryTitle>
-      <BlocSize70>
-        <Logo15 src={dekra} />
-        <Logo15 src={nes} />
-        <Logo15 src={cif} />
-        <Logo15 src={lagglo} />
-      </BlocSize70>
-
-      <CategoryTitle>Industries</CategoryTitle>
-      <BlocSize70>
-        <Logo15 src={elvia} />
-        <Logo15 src={peg} />
-        <Logo15 src={gaultfremont} />
-        <Logo15 src={clariant} />
-      </BlocSize70>
-
-      <CategoryTitle>Agro-alimentaire</CategoryTitle>
-      <BlocAgro>
-        <Logo src={elivia} />
-      </BlocAgro>
+              {partner.logos.map((logo) => {
+                return <Logo15 src={logo.src} alt={logo.alt} />;
+              })}
+            </Bloc1>
+          );
+        })}
     </Main>
   );
 };
