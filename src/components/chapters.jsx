6e3156a3f8bc5bf1110947683b6db6179bc02c93
AlexisCatrix <alexis.4x.ac@gmail.com>
2021-01-06 10:57:31 +0100

    Create sections chapter

---
+++ src/components/chapters.jsx
@@ -0,0 +1,17 @@
+function Chapters() {
+  return (
+    <div>
+      <div>
+        <SectionChapter>Chapitre 1</SectionChapter>
+      </div>
+      <div>
+        <SectionChapter>Chapitre 2</SectionChapter>
+      </div>
+      <div>
+        <SectionChapter>Chapitre 3</SectionChapter>
+      </div>
+    </div>
+  );
+}
+
+export default Chapters;
