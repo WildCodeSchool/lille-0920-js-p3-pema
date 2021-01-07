2deaff7a95c47020700ede2024e479acc646a220
Sarahshr <saraah.sahraoui@gmail.com>
2021-01-07 16:23:24 +0100

    Revert "Resolve conflicts"
    This reverts commit afcbac7e9b36d2414cfc5d0e00f01cbedfb5b046, reversing
    changes made to 69d9cc25a1bf020640561ec2ab459f11cc418587.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import Partners from "./Components/Partners";
-import Header from "./Components/Header/Header";
 import Timeline from "./Components/Timeline/Timeline";
 import { Reset } from "styled-reset";
 
@@ -7,7 +6,6 @@ export default function App() {
   return (
     <>
       <Reset />
-      <Header />
       <Timeline />
       <Partners />
     </>
