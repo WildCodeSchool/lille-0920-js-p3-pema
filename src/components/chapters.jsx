03e4ebd26e6e71dc439801f45f9ff991cb1fecf4
AlexisCatrix <alexis.4x.ac@gmail.com>
2021-01-07 12:45:07 +0100

    Fix problem about render of modal's text depending on the event on the clicked block

--- src/components/chapters.jsx
+++ src/components/chapters.jsx
@@ -5,64 +5,60 @@ import {
   Chapter,
   BackgroundImageChapter,
   Modal,
-  ModalAndChapterContainer
+  ModalAndChapterContainer,
+  TabPage
 } from "../styled-components/ChaptersStyled";
 
 function Chapters() {
-  const [openModal1, setOpenModal1] = useState(false);
-  const [openModal2, setOpenModal2] = useState(false);
-  const [openModal3, setOpenModal3] = useState(false);
+  const [openModal, setOpenModal] = useState({
+    modal1: "id1",
+    modal2: "id2",
+    modal3: "id3",
+  });
 
   return (
-    <Container>
-      <ModalAndChapterContainer>
-        <ChapterContainer onClick={() => setOpenModal1(!openModal1)}>
-          <BackgroundImageChapter
-            openModal={openModal1}
-            src="https://via.placeholder.com/300"
-          />
-          <Chapter>Chapitre 1</Chapter>
-        </ChapterContainer>
-        <Modal>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
-          sint nihil sunt error consectetur neque saepe dolorum voluptates,
-          aspernatur architecto, labore totam similique voluptas! Itaque quam
-          saepe similique numquam inventore.
-        </Modal>
-      </ModalAndChapterContainer>
+    <TabPage>
+      <Container>
+        <ModalAndChapterContainer>
+          <ChapterContainer onClick={() => setOpenModal("id1")}>
+            <BackgroundImageChapter src="https://via.placeholder.com/300" />
+            <Chapter>Chapitre 1</Chapter>
+          </ChapterContainer>
+        </ModalAndChapterContainer>
 
-      <ModalAndChapterContainer>
-        <ChapterContainer onClick={() => setOpenModal2(!openModal2)}>
-          <BackgroundImageChapter
-            openModal={openModal2}
-            src="https://via.placeholder.com/300"
-          />
-          <Chapter>Chapitre 2</Chapter>
-        </ChapterContainer>
-        <Modal>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti ab
-          amet odit itaque, illo voluptates eligendi cupiditate a officiis
-          excepturi dolores nulla quibusdam illum, ipsa eveniet, aliquid
-          deleniti enim repellendus.
-        </Modal>
-      </ModalAndChapterContainer>
+        <ModalAndChapterContainer>
+          <ChapterContainer onClick={() => setOpenModal("id2")}>
+            <BackgroundImageChapter src="https://via.placeholder.com/300" />
+            <Chapter>Chapitre 2</Chapter>
+          </ChapterContainer>
+        </ModalAndChapterContainer>
 
-      <ModalAndChapterContainer>
-        <ChapterContainer onClick={() => setOpenModal3(!openModal3)}>
-          <BackgroundImageChapter
-            openModal={openModal3}
-            src="https://via.placeholder.com/300"
-          />
-          <Chapter>Chapitre 3</Chapter>
-        </ChapterContainer>
-        <Modal>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae
-          obcaecati in voluptatum, asperiores quidem quae harum explicabo at nam
-          unde soluta pariatur! Quisquam maiores porro temporibus eaque et?
-          Odio, libero.
-        </Modal>
-      </ModalAndChapterContainer>
-    </Container>
+        <ModalAndChapterContainer>
+          <ChapterContainer onClick={() => setOpenModal("id3")}>
+            <BackgroundImageChapter src="https://via.placeholder.com/300" />
+            <Chapter>Chapitre 3</Chapter>
+          </ChapterContainer>
+        </ModalAndChapterContainer>
+      </Container>
+      <Modal className={openModal === "id1" ? "visible" : "hidden"}>
+        Chapitre 1 : Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Recusandae sint nihil sunt error consectetur neque saepe dolorum
+        voluptates, aspernatur architecto, labore totam similique voluptas!
+        Itaque quam saepe similique numquam inventore.
+      </Modal>
+      <Modal className={openModal === "id2" ? "visible" : "hidden"}>
+        Chapitre 2 : Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Corrupti ab amet odit itaque, illo voluptates eligendi cupiditate a
+        officiis excepturi dolores nulla quibusdam illum, ipsa eveniet, aliquid
+        deleniti enim repellendus.
+      </Modal>
+      <Modal className={openModal === "id3" ? "visible" : "hidden"}>
+        Chapitre 3 : Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        Beatae obcaecati in voluptatum, asperiores quidem quae harum explicabo
+        at nam unde soluta pariatur! Quisquam maiores porro temporibus eaque et?
+        Odio, libero.
+      </Modal>
+    </TabPage>
   );
 }
 
