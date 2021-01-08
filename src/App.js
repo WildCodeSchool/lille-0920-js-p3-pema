6ae03898e04f7842be81cb465f42d674d06c93dd
AlexisCatrix <alexis.4x.ac@gmail.com>
2021-01-08 22:14:46 +0100

    Add styled-reset component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Chapters from "./components/chapters";
+import {Reset} from "styled-reset";
 
 export default function App() {
   return (
     <>
+      <Reset />
       <Chapters />
     </>
   );
