35bf1a119a39139abd96032406a8a07f6ed9057f
AlexisCatrix <alexis.4x.ac@gmail.com>
2021-01-10 00:21:02 +0100

    Create shortcut to change modal when user is scrolling page

--- src/components/chapters.jsx
+++ src/components/chapters.jsx
@@ -10,6 +10,9 @@ import {
   Title,
   SchemaContainer,
   BackgroundColor,
+  ShortcutChapter,
+  ShortcutContainer,
+  ChapterTitle,
 } from "../styled-components/ChaptersStyled";
 import tabl1 from "../assets/pictures/tabl1.png";
 import tabl2 from "../assets/pictures/tabl2.png";
@@ -27,8 +30,52 @@ function Chapters() {
   return (
     <BackgroundColor className={openModal.isActive === true ? "grow" : ""}>
       <TabPage>
+        <ShortcutContainer
+          className={
+            openModal.modal1 === "id1"
+              ? "borderBlue"
+              : openModal.modal2 === "id2"
+              ? "borderGold"
+              : "borderRed"
+          }
+        >
+          <ChapterTitle
+            onClick={() => setOpenModal({ modal1: "id1", isActive: true })}
+            className={
+              openModal.modal1 === "id1"
+                ? "backgroundBlue"
+                : "backgroundBlueOpacity"
+            }
+          >
+            Chapitre 1
+          </ChapterTitle>
+
+          <ChapterTitle
+            onClick={() => setOpenModal({ modal2: "id2", isActive: true })}
+            className={
+              openModal.modal2 === "id2"
+                ? "backgroundGold"
+                : "backgroundGoldOpacity"
+            }
+          >
+            Chapitre 2
+          </ChapterTitle>
+
+          <ChapterTitle
+            onClick={() => setOpenModal({ modal3: "id3", isActive: true })}
+            className={
+              openModal.modal3 === "id3"
+                ? "backgroundRed"
+                : "backgroundRedOpacity"
+            }
+          >
+            Chapitre 3
+          </ChapterTitle>
+        </ShortcutContainer>
         <TabContainer>
-          <ChapterContainer onClick={() => setOpenModal({modal1:"id1", isActive: true})}>
+          <ChapterContainer
+            onClick={() => setOpenModal({ modal1: "id1", isActive: true })}
+          >
             <BackgroundImageChapter
               className={openModal.modal1 === "id1" ? "borderBlue" : ""}
               src={tabl1}
@@ -36,7 +83,9 @@ function Chapters() {
             <Chapter>Un voyage inédit au coeur de votre entreprise</Chapter>
           </ChapterContainer>
 
-          <ChapterContainer onClick={() => setOpenModal({modal2:"id2", isActive: true})}>
+          <ChapterContainer
+            onClick={() => setOpenModal({ modal2: "id2", isActive: true })}
+          >
             <BackgroundImageChapter
               className={openModal.modal2 === "id2" ? "borderGold" : ""}
               src={tabl2}
@@ -44,7 +93,9 @@ function Chapters() {
             <Chapter>Carnet de voyage pratique</Chapter>
           </ChapterContainer>
 
-          <ChapterContainer onClick={() => setOpenModal({modal3:"id3", isActive: true})}>
+          <ChapterContainer
+            onClick={() => setOpenModal({ modal3: "id3", isActive: true })}
+          >
             <BackgroundImageChapter
               className={openModal.modal3 === "id3" ? "borderRed" : ""}
               src={tabl3}
@@ -52,111 +103,128 @@ function Chapters() {
             <Chapter>Témoignages</Chapter>
           </ChapterContainer>
         </TabContainer>
-        <BackgroundModal className={openModal.modal1 === "id1" ? "borderBlue" : ""}>
+        <BackgroundModal
+          className={openModal.modal1 === "id1" ? "borderBlue" : ""}
+        >
           <Modal className={openModal.modal1 === "id1" ? "visible" : "hidden"}>
             <Title>
               Chapitre 1 : Un voyage inédit au coeur de votre entreprise
             </Title>
-            Les problèmes, les défis, les challenges, sont autant d’opportunités
-            pour découvrir ou redécouvrir les richesses insoupçonnées de votre
-            entreprise. La principale d’entre elle est votre équipe.
-            <br />
-            <br />
-            Trop souvent, « l’esprit d’équipe » est perdu. Pour diverses raisons
-            : un management inapproprié, le travail en silos, les conflits
-            d’intérêt, les déceptions, l’histoire de l’entreprise, le manque de
-            sens… Lorsque l’esprit d’équipe est retrouvé, les motivations
-            reviennent, les idées naissent, les initiatives fleurissent, la
-            confiance et la solidarité se développent.
-            <br />
-            <br />
-            Les problèmes et les défis sont portés par toute l’équipe et plus
-            seulement par son dirigeant ou quelques managers. Vos processus
-            deviennent plus simples, plus robustes, plus agiles. Des résultats
-            émergent rapidement. Le cercle vertueux se met en place. La culture
-            de l’entreprise change.
-            <br />
-            <br />
-            <SchemaContainer>
-              <img src={schema1} alt="schéma motivation entreprise" />
-            </SchemaContainer>
-            <br />
-            <br />
-            schema2
-            <br />
-            Mais nous sommes fiers des résultats qui en ont découlés chez nos
-            clients. Nous sommes fiers d’avoir redécouvert que les choses les
-            plus simples sont les plus efficaces. Nous sommes fiers de remettre
-            l’humain au cœur de l’entreprise et de sa performance.
-            <br />
-            <br />
-            Une question demeure : comment fait-on ? Par où commencer ? En
-            préparant son carnet de voyage ! Bien sûr.
+            <p>
+              Les problèmes, les défis, les challenges, sont autant
+              d’opportunités pour découvrir ou redécouvrir les richesses
+              insoupçonnées de votre entreprise. La principale d’entre elle est
+              votre équipe.
+              <br />
+              <br />
+              Trop souvent, « l’esprit d’équipe » est perdu. Pour diverses
+              raisons : un management inapproprié, le travail en silos, les
+              conflits d’intérêt, les déceptions, l’histoire de l’entreprise, le
+              manque de sens… Lorsque l’esprit d’équipe est retrouvé, les
+              motivations reviennent, les idées naissent, les initiatives
+              fleurissent, la confiance et la solidarité se développent.
+              <br />
+              <br />
+              Les problèmes et les défis sont portés par toute l’équipe et plus
+              seulement par son dirigeant ou quelques managers. Vos processus
+              deviennent plus simples, plus robustes, plus agiles. Des résultats
+              émergent rapidement. Le cercle vertueux se met en place. La
+              culture de l’entreprise change.
+              <br />
+              <br />
+              <SchemaContainer>
+                <img src={schema1} alt="schéma motivation entreprise" />
+              </SchemaContainer>
+              <br />
+              <br />
+              schema2
+              <br />
+              Mais nous sommes fiers des résultats qui en ont découlés chez nos
+              clients. Nous sommes fiers d’avoir redécouvert que les choses les
+              plus simples sont les plus efficaces. Nous sommes fiers de
+              remettre l’humain au cœur de l’entreprise et de sa performance.
+              <br />
+              <br />
+              Une question demeure : comment fait-on ? Par où commencer ? En
+              préparant son carnet de voyage ! Bien sûr.
+            </p>
           </Modal>
         </BackgroundModal>
 
-        <BackgroundModal className={openModal.modal2 === "id2" ? "borderGold" : ""}>
+        <BackgroundModal
+          className={openModal.modal2 === "id2" ? "borderGold" : ""}
+        >
           <Modal className={openModal.modal2 === "id2" ? "visible" : "hidden"}>
             <Title>Chapitre 2 : Carnet de voyage</Title>
-            Que dire ?
-            <br />
-            <br />
-            Si chaque entreprise est unique, son voyage sera aussi unique. Mais
-            il y a des points de passage obligés. Des leviers importants. Ils
-            sont au nombre de 4.
-            <br />
-            <br />
-            <SchemaContainer>
-              <img src={schema2} alt="vision d'entreprise" />
-            </SchemaContainer>
-            <br />
-            <br />
-            Sans doute avez-vous déjà entendu parler de tout cela ?
-            <br />
-            <br />
-            Sans doute avez-vous le sentiment que vous connaissez ces leviers et
-            que vous les avez déjà essayés ?
-            <br />
-            <br />
-            Sans doute avez-vous une bonne vision, une bonne stratégie pour
-            votre entreprise ?
-            <br />
-            <br />
-            Sans doute avez-vous à maintes reprises essayé de vous appuyer sur
-            vos équipes ? Sans succès…
-            <br />
-            <br />
-            Sans doute estimez-vous que votre manière de manager est la seule
-            qui soit efficace pour votre entreprise.
-            <br />
-            <br />
-            Enfin, peut-être avez-vous déjà essayé d’être accompagné par un
-            consultant brillant, ou avez-vous déjà financé des formations au
-            management, au leadership… pour quels résultats ?
-            <br />
-            <br />
-            Vous avez raison. Rien de nouveau sous le soleil.
-            <br />
-            <br />
-            <br />
-            <br />
-            Qu’aurions-nous de différent ?
+            <p>
+              Que dire ?
+              <br />
+              <br />
+              Si chaque entreprise est unique, son voyage sera aussi unique.
+              Mais il y a des points de passage obligés. Des leviers importants.
+              Ils sont au nombre de 4.
+              <br />
+              <br />
+              <SchemaContainer>
+                <img src={schema2} alt="vision d'entreprise" />
+              </SchemaContainer>
+              <br />
+              <br />
+              Sans doute avez-vous déjà entendu parler de tout cela ?
+              <br />
+              <br />
+              Sans doute avez-vous le sentiment que vous connaissez ces leviers
+              et que vous les avez déjà essayés ?
+              <br />
+              <br />
+              Sans doute avez-vous une bonne vision, une bonne stratégie pour
+              votre entreprise ?
+              <br />
+              <br />
+              Sans doute avez-vous à maintes reprises essayé de vous appuyer sur
+              vos équipes ? Sans succès…
+              <br />
+              <br />
+              Sans doute estimez-vous que votre manière de manager est la seule
+              qui soit efficace pour votre entreprise.
+              <br />
+              <br />
+              Enfin, peut-être avez-vous déjà essayé d’être accompagné par un
+              consultant brillant, ou avez-vous déjà financé des formations au
+              management, au leadership… pour quels résultats ?
+              <br />
+              <br />
+              Vous avez raison. Rien de nouveau sous le soleil.
+              <br />
+              <br />
+              <br />
+              <br />
+              Qu’aurions-nous de différent ?
+            </p>
           </Modal>
         </BackgroundModal>
 
-        <BackgroundModal className={openModal.modal3 === "id3" ? "borderRed" : ""}>
+        <BackgroundModal
+          className={openModal.modal3 === "id3" ? "borderRed" : ""}
+        >
           <Modal className={openModal.modal3 === "id3" ? "visible" : "hidden"}>
             <Title>Chapitre 3 : Souvenir du voyage</Title>
-            Quelques exemples valent mieux qu'un long discours, n'est-ce pas ?
-            <br />
-            <br />
-            <SchemaContainer>
-              <img
-                id="carrousel"
-                src="https://via.placeholder.com/1150x500?text=Place+carrousel+here"
-                alt="carrousel"
-              />
-            </SchemaContainer>
+            <p>
+              Quelques exemples valent mieux qu'un long discours, n'est-ce pas ?
+              <br />
+              <br />
+              <SchemaContainer>
+                <img
+                  id="carrousel"
+                  src="https://via.placeholder.com/1250x500?text=Place+carrousel+here"
+                  alt="carrousel"
+                />
+              </SchemaContainer>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste,
+              tenetur! Aut facere soluta enim possimus, officiis labore totam
+              repellat qui omnis consequuntur corporis similique iure dicta
+              impedit ullam id ipsum.
+            </p>
           </Modal>
         </BackgroundModal>
       </TabPage>
