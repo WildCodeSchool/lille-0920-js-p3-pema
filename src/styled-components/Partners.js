b9fab817307ed47f02317140d29cecf97bd4c1c4
Sarahshr <saraah.sahraoui@gmail.com>
2021-01-11 15:37:01 +0100

    Move Partners.jsx on Partner folder and change the style

--- src/styled-components/Partners.js
+++ src/styled-components/Partners.js
@@ -1,54 +1,46 @@
 import styled from "styled-components";
 
 export const Main = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  font-family: GT Walsheim;
+  text-align: center;
+  width: 100%;
+  margin-bottom: 10%;
+  border-top: 1px solid grey;
+  border-bottom: 1px solid grey;
 `;
 
 export const Title = styled.h2`
   font-size: 30pt;
+  margin: 5%;
   font-weight: lighter;
   color: #001027;
   @media screen and (max-width: 500px) {
-    font-size: 20pt;
+    font-size: 18pt;
   }
-`;
-
-export const CategoryTitle = styled.span`
-  display: ${({ id }) => (id === 0 ? "none" : "flex")};
-  font-size: 18pt;
-  padding-bottom: 5%;
-  color: #001027;
-  @media screen and (max-width: 500px) {
-    font-size: 15pt;
-  }
-`;
-
-export const BlocsContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  flex-wrap: wrap;
 `;
 
 export const Blocs = styled.div`
   display: flex;
-  flex-wrap: wrap;
-  flex-direction: ${({ id }) => (id === 0 ? "row" : "column")};
-  justify-content: center;
-  width: ${({ id }) => (id === 0 ? "80%" : "30%")};
-  align-items: center;
+  flex-direction: column;
+`;
+
+export const CategoryTitle = styled.span`
+  font-size: 18pt;
+  margin-bottom: 2%;
+  visibility: ${({ id }) => id === 0 && "hidden"};
   @media screen and (max-width: 500px) {
-    width: ${({ id }) => (id === 0 ? "100%" : "50%")};
+    font-size: 10pt;
   }
 `;
 
 export const Logo = styled.img`
   width: ${({ id }) =>
-    (id > 0 && id < 4 && "35%") ||
-    (id > 3 && id < 7 && "25%") ||
+    (id > 0 && id < 4 && "15%") ||
+    (id > 3 && id < 7 && "10%") ||
     (id === 0 && "15%")};
-  margin: ${({ id }) => (id > 0 && id < 4 ? "0" : "3%")};
-  margin-top: ${({ id }) => (id === 3 ? "5%" : "0")};
+`;
+
+export const Logos = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
 `;
