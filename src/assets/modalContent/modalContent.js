0ea0703c3d444b389c3bb70797bbd4e8644c917d
AlexisCatrix <alexis.4x.ac@gmail.com>
2021-01-18 15:34:46 +0100

    Move modal's content in a array for a better visibility in Chapters component

---
+++ src/assets/modalContent/modalContent.js
@@ -0,0 +1,99 @@
+import schema1 from "../pictures/schema1_chapter_1.png";
+import schema2 from "../pictures/schema2_chapitre_2.png";
+
+export const modalContent = [
+  [
+    {
+      modalItem: 1,
+      title: "Chapitre 1 : Un voyage inédit au coeur de votre entreprise",
+      paragraph: [
+        {
+          id: 1,
+          text:
+            "Les problèmes, les défis, les challenges, sont autant d’opportunités pour découvrir ou redécouvrir les richesses insoupçonnées de votre entreprise.",
+        },
+        { id: 2, text: "La principale d’entre elle est votre équipe." },
+        {
+          id: 3,
+          text:
+            "Trop souvent, « l’esprit d’équipe » est perdu. Pour diverses raisons : un management inapproprié, le travail en silos, les conflits d’intérêt, les déceptions, l’histoire de l’entreprise, le manque de sens…",
+        },
+        {
+          id: 4,
+          text:
+            "Lorsque l’esprit d’équipe est retrouvé, les motivations reviennent, les idées naissent, les initiatives fleurissent, la confiance et la solidarité se développent. Les problèmes et les défis sont portés par toute l’équipe et plus seulement par son dirigeant ou quelques managers.",
+        },
+        {
+          id: 5,
+          text:
+            "Le cercle vertueux se met en place. La culture de l’entreprise change.",
+          schema: { src: schema1, alt: "company vision" },
+        },
+        {
+          id: 6,
+          text:
+            "La première fois que nous avons déployé un projet de transformation ainsi, nous étions animés par des convictions un peu naïves, pour ne pas dire « bisounours », et il est légitime de penser qu’il peut en être ainsi.",
+        },
+        {
+          id: 7,
+          text:
+            "Mais nous sommes fiers des résultats qui en ont découlés chez nos clients. Nous sommes fiers d’avoir redécouvert que les choses les plus simples sont les plus efficaces. Nous sommes fiers de remettre l’humain au cœur de l’entreprise et de sa performance.",
+        },
+        {
+          id: 8,
+          text:
+            "Une question demeure : comment fait-on ? Par où commencer ? En préparant son carnet de voyage ! Bien sûr.",
+        },
+      ],
+    },
+  ],
+  [
+    {
+      modalItem: 2,
+      title: "Chapitre 2 : Carnet de voyage",
+      subtitle: "Que dire ?",
+      paragraph: [
+        {
+          id: 1,
+          text:
+            "Si chaque entreprise est unique, son voyage sera aussi unique. Mais il y a des points de passage obligés. Des leviers importants. Ils sont au nombre de 4.",
+          schema: { src: schema2, alt: "company vision" },
+        },
+        {
+          id: 2,
+          text: "Sans doute avez-vous déjà entendu parler de tout cela ?",
+        },
+
+        {
+          id: 3,
+          text:
+            "Sans doute avez-vous le sentiment que vous connaissez ces leviers et que vous les avez déjà essayés ?",
+        },
+        {
+          id: 4,
+          text:
+            "Sans doute avez-vous une bonne vision, une bonne stratégie pour votre entreprise ?",
+        },
+        {
+          id: 5,
+          text:
+            "Sans doute avez-vous à maintes reprises essayé de vous appuyer sur vos équipes ? Sans succès…",
+        },
+        {
+          id: 6,
+          text:
+            "Sans doute estimez-vous que votre manière de manager est la seule qui soit efficace pour votre entreprise.",
+        },
+        {
+          id: 7,
+          text:
+            "Enfin, peut-être avez-vous déjà essayé d’être accompagné par un consultant brillant, ou avez-vous déjà financé des formations au management, au leadership… pour quels résultats ?",
+        },
+        {
+          id: 8,
+          text: "Vous avez raison. Rien de nouveau sous le soleil.",
+        },
+      ],
+    },
+  ],
+];
