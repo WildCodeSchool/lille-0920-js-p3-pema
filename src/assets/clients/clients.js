ea28e3c5251fe5a677c07e18a96b506e36b640e7
AlexisCatrix <alexis.4x.ac@gmail.com>
2021-01-19 22:25:29 +0100

    Import pictures and create data clients to map information in chapter three

---
+++ src/assets/clients/clients.js
@@ -0,0 +1,67 @@
+import sophartex from "./images/sophartex.jpeg";
+import unither from "./images/unither.png";
+import legendre from "./images/legendre.png";
+import alimakek from "./images/alimakek.png";
+import locapal from "./images/locapal.gif";
+import elivia from "./images/elivia.png";
+import dekra from "./images/dekra.png";
+import nes from "./images/nes.png";
+import cif from "./images/cif.png";
+import lagglo from "./images/lagglo.jpeg";
+import csgroup from "./images/cs-group.png";
+import cacom from "./images/cacom.png";
+import peg from "./images/peg.jpeg";
+import gaultfremont from "./images/gault-fremont.png";
+import clariant from "./images/clariant.png";
+
+export const myClients = [
+  {
+    id: 1,
+    name: "Pharmacie",
+    logos: [
+      { src: sophartex, alt: "sophartex" },
+      { src: unither, alt: "unither" },
+    ],
+  },
+  {
+    id: 2,
+    name: "Construction",
+    logos: [
+      { src: legendre, alt: "legendre" },
+      { src: alimakek, alt: "alimakek" },
+      { src: locapal, alt: "locapal" },
+    ],
+  },
+  {
+    id: 3,
+    name: "IT",
+    logos: [
+      { src: csgroup, alt: "csgroup" },
+      { src: cacom, alt: "cacom" },
+    ],
+  },
+  {
+    id: 4,
+    name: "Services",
+    logos: [
+      { src: dekra, alt: "dekra" },
+      { src: nes, alt: "nes" },
+      { src: cif, alt: "cif" },
+      { src: lagglo, alt: "lagglo" },
+    ],
+  },
+  {
+    id: 5,
+    name: "Industries",
+    logos: [
+      { src: peg, alt: "peg" },
+      { src: gaultfremont, alt: "gaultfremont" },
+      { src: clariant, alt: "clariant" },
+    ],
+  },
+  {
+    id: 6,
+    name: "Agro-alimentaire",
+    logos: [{ src: elivia, alt: "elivia" }],
+  },
+];
