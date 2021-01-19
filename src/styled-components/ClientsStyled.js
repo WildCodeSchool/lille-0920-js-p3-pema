bc3879388efc4446767b41bc7ec10b0604c93e2d
AlexisCatrix <alexis.4x.ac@gmail.com>
2021-01-19 22:28:59 +0100

    Add some style to clients component

---
+++ src/styled-components/ClientsStyled.js
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+export const Logo = styled.img`
+  width: auto;
+  height: 100px;
+  margin: 2vw;
+`;
+
+export const CategoryName = styled.h1`
+    font-size: 1.5em;
+    font-weight: bold;
+    margin: 2vh;
+    padding-left: 1vw;
+    color: white;
+    text-shadow: 2px 2px 4px #000;
+    background: linear-gradient(170deg, #ce4b4b, white);
+`;
+
+export const DisplayLogo = styled.div`
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-template-rows: repeat(auto, 1fr);
+    align-items: center;
+    align-self: start;
+`;
+
+export const ClientsContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin: 2vh;
+    border: 2px solid #ce4b4b;
+`;
