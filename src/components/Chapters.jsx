a562d3d3ee1a5956d6f800feb53f2aeb9997df46
AlexisCatrix <alexis.4x.ac@gmail.com>
2021-01-20 14:43:13 +0100

    Rename styled components according new format

--- src/components/Chapters.jsx
+++ src/components/Chapters.jsx
@@ -1,86 +1,77 @@
 import {
-  Modal,
+  Section,
   ParagraphOneSpacing,
   Schema,
   SchemaContainer,
-  TabPage,
+  SectionContainer,
   Title,
 } from "../styled-components/ChaptersStyled";
 import Clients from "./Clients";
 import Timeline from "./Timeline";
 import { modalContent } from "../assets/modalContent/modalContent";
 
-function Chapters({ openModal, displayShortcut }) {
+function Chapters() {
   return (
-      <TabPage id="topOfPage">
-        <Modal
-          className={openModal.modal === "id1" ? "visible" : "hidden"}
-          id={displayShortcut === false ? "hideShortcut" : "showShortcut"}
-        >
-          {modalContent[0].map((content) => (
-            <div key={content.modalItem}>
-              <Title>{content.title}</Title>
-              {content.paragraph.map((textContent) => (
-                <div key={textContent.id}>
-                  <ParagraphOneSpacing>{textContent.text}</ParagraphOneSpacing>
-                  {textContent.schema && (
-                    <SchemaContainer>
-                      <Schema
-                        src={textContent.schema.src}
-                        alt={textContent.schema.alt}
-                      />
-                    </SchemaContainer>
-                  )}
-                </div>
-              ))}
-            </div>
-          ))}
-        </Modal>
+    <SectionContainer>
+      <Section>
+      {modalContent[0].map((content) => (
+          <div key={content.modalItem}>
+            <Title>{content.title}</Title>
+            {content.paragraph.map((textContent) => (
+              <div key={textContent.id}>
+                <ParagraphOneSpacing>{textContent.text}</ParagraphOneSpacing>
+                {textContent.schema && (
+                  <SchemaContainer>
+                    <Schema
+                      src={textContent.schema.src}
+                      alt={textContent.schema.alt}
+                    />
+                  </SchemaContainer>
+                )}
+              </div>
+            ))}
+          </div>
+        ))}
+      </Section>
 
-        <Modal
-          className={openModal.modal === "id2" ? "visible" : "hidden"}
-          id={displayShortcut === false ? "hideShortcut" : "showShortcut"}
-        >
-          {modalContent[1].map((content) => (
-            <div key={content.modalItem}>
-              <Title>{content.title}</Title>
-              <Title id="subtitle">{content.subtitle}</Title>
-              {content.paragraph.map((textContent) => (
-                <div key={textContent.id}>
-                  <ParagraphOneSpacing>{textContent.text}</ParagraphOneSpacing>
-                  {textContent.schema && (
-                    <SchemaContainer>
-                      <Schema
-                        src={textContent.schema.src}
-                        alt={textContent.schema.alt}
-                      />
-                    </SchemaContainer>
-                  )}
-                </div>
-              ))}
-            </div>
-          ))}
-          <Title> Qu’aurions-nous de différent ?</Title>
-          <Timeline />
-        </Modal>
+      <Section>
+        {modalContent[1].map((content) => (
+          <div key={content.modalItem}>
+            <Title>{content.title}</Title>
+            <Title id="subtitle">{content.subtitle}</Title>
+            {content.paragraph.map((textContent) => (
+              <div key={textContent.id}>
+                <ParagraphOneSpacing>{textContent.text}</ParagraphOneSpacing>
+                {textContent.schema && (
+                  <SchemaContainer>
+                    <Schema
+                      src={textContent.schema.src}
+                      alt={textContent.schema.alt}
+                    />
+                  </SchemaContainer>
+                )}
+              </div>
+            ))}
+          </div>
+        ))}
+        <Title> Qu’aurions-nous de différent ?</Title>
+        <Timeline />
+      </Section>
 
-        <Modal
-          className={openModal.modal === "id3" ? "visible" : "hidden"}
-          id={displayShortcut === false ? "hideShortcut" : "showShortcut"}
-        >
-          {modalContent[2].map((content) => (
-            <div key={content.modalItem}>
-              <Title>{content.title}</Title>
-              {content.paragraph.map((textContent) => (
-                <div key={textContent.id}>
-                  <ParagraphOneSpacing>{textContent.text}</ParagraphOneSpacing>
-                </div>
-              ))}
-            </div>
-          ))}
-          <Clients />
-        </Modal>
-      </TabPage>
+      <Section>
+        {modalContent[2].map((content) => (
+          <div key={content.modalItem}>
+            <Title>{content.title}</Title>
+            {content.paragraph.map((textContent) => (
+              <div key={textContent.id}>
+                <ParagraphOneSpacing>{textContent.text}</ParagraphOneSpacing>
+              </div>
+            ))}
+          </div>
+        ))}
+        <Clients />
+      </Section>
+    </SectionContainer>
   );
 }
 
