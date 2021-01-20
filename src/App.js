4c9db4b9c1b3f52d1b2ff0ad6d2374156ec1d37e
AlexisCatrix <alexis.4x.ac@gmail.com>
2021-01-20 10:36:14 +0100

    Lift up the state and pass states in components' props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
+import React, { useState } from "react";
 import Chapters from "./components/Chapters";
-import {Reset} from "styled-reset";
+import { Reset } from "styled-reset";
+import ShortcutChapter from "./components/ShortcutChapter";
+import { AppContainer, BackgroundColor } from "./styled-components/AppStyled";
 
 export default function App() {
+  const [openModal, setOpenModal] = useState({ modal: "id1" });
+  const [displayShortcut, setDisplayShortcut] = useState(true);
   return (
-    <>
-      <Reset />
-      <Chapters />
-    </>
+    <BackgroundColor>
+      <AppContainer>
+        <Reset />
+        <ShortcutChapter
+          openModal={openModal}
+          setOpenModal={setOpenModal}
+          displayShortcut={displayShortcut}
+          setDisplayShortcut={setDisplayShortcut}
+        />
+        <Chapters
+          openModal={openModal}
+          setOpenModal={setOpenModal}
+          displayShortcut={displayShortcut}
+          setDisplayShortcut={setDisplayShortcut}
+        />
+      </AppContainer>
+    </BackgroundColor>
   );
 }
