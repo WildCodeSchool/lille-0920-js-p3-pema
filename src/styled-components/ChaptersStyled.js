8546731c134c9850e73d48415873978eb136887a
AlexisCatrix <alexis.4x.ac@gmail.com>
2021-01-20 14:46:07 +0100

    Remove useless styled components and change render of document

--- src/styled-components/ChaptersStyled.js
+++ src/styled-components/ChaptersStyled.js
@@ -1,6 +1,20 @@
 import styled from "styled-components";
 
-export const Modal = styled.div`
+
+export const ParagraphOneSpacing = styled.p`
+  line-height: 2em;
+`;
+
+export const Schema = styled.img`
+  margin: 4vh;
+`;
+
+export const SchemaContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;  
+
+export const Section = styled.div`
   &.visible {
     display: flex;
     flex-direction: column;
@@ -23,33 +37,12 @@ export const Modal = styled.div`
   }
 `;
 
-export const ParagraphOneSpacing = styled.p`
-  line-height: 2em;
-`;
-
-export const Schema = styled.img`
-  margin: 4vh;
-`;
-
-export const SchemaContainer = styled.div`
+export const SectionContainer = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
-`;
-
-export const TabContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-`;
-
-export const ChapterContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-export const TabPage = styled.div`
-  display: flex;
-  margin: 3vw 5vw 3vw 1vw;
+  width: 95vw;
+  margin: 3vw;
 `;
 
 export const Title = styled.h1`
@@ -57,6 +50,7 @@ export const Title = styled.h1`
   font-weight: bold;
   line-height: 3em;
   color: black;
+  margin: 5vh 0;
 
   &#subtitle {
     font-weight: unset;
