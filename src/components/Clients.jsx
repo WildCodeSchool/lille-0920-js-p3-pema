3241ce85c1c1bb3c53d8b3be59a3eb9d31916907
Thomas <thomas.mulhauser@viacesi.fr>
2021-01-20 16:08:40 +0100

    sort array

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -9,18 +9,22 @@ import {
 export default function Clients() {
   return (
     <div>
-      {myClients.map((client) => (
-        <div key={client.id}>
-          <CategoryName>{client.name}</CategoryName>
-          <ClientsContainer>
-            {client.logos.map((logo) => (
-              <DisplayLogo key={logo.alt}>
-                <Logo src={logo.src} alt={logo.alt} id={client.id} />
-              </DisplayLogo>
-            ))}
-          </ClientsContainer>
-        </div>
-      ))}
+      {myClients
+        .sort(function (a, b) {
+          return b.logos.length - a.logos.length;
+        })
+        .map((client) => (
+          <div key={client.id}>
+            <CategoryName>{client.name}</CategoryName>
+            <ClientsContainer>
+              {client.logos.map((logo) => (
+                <DisplayLogo key={logo.alt}>
+                  <Logo src={logo.src} alt={logo.alt} id={client.id} />
+                </DisplayLogo>
+              ))}
+            </ClientsContainer>
+          </div>
+        ))}
     </div>
   );
 }
