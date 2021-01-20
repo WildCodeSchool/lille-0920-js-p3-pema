2e7cd0caf1713e248cba298b72948e52fafa38c3
AlexisCatrix <alexis.4x.ac@gmail.com>
2021-01-20 10:32:13 +0100

    Export ShortcutChapter style from ChaptersStyled.js and change some value to adapt render

--- src/styled-components/ChaptersStyled.js
+++ src/styled-components/ChaptersStyled.js
@@ -1,49 +1,5 @@
 import styled from "styled-components";
 
-export const BackgroundColor = styled.div`
-  display: flex;
-  align-items: center;
-  background: linear-gradient(180deg, white, black);
-  height: auto;
-  padding-bottom: 5vh;
-`;
-
-export const ChapterTitle = styled.h1`
-  display: flex;
-  justify-content: center;
-  font-size: 2em;
-  line-height: 50px;
-  color: white;
-  text-shadow: 2px 2px 4px #000;
-  width: 350px;
-  &:hover {
-    background: rgba(135, 135, 135);
-    transition: all 0.35s ease-in;
-  }
-  &.isClicked {
-    background: rgba(135, 135, 135);
-  }
-`;
-
-export const HideShortcut = styled.div`
-  padding: 0.5em;
-  border-radius: 10px;
-  width: min-content;
-`;
-
-export const LogoHideShortcut = styled.i`
-  font-size: 30px;
-  color: white;
-  &#rotateLeft {
-    transform: rotate(180deg);
-    transition: all 0.2s ease-in;
-  }
-  &#rotateRight {
-    transform: rotate(360deg);
-    transition: all 0.2s ease-in;
-  }
-`;
-
 export const Modal = styled.div`
   &.visible {
     display: flex;
@@ -51,15 +7,15 @@ export const Modal = styled.div`
     background: white;
     height: max-content;
     width: 65vw;
-    padding: 2em;
-    margin: 0.5em;
+    padding: 2vw;
+    margin: 2vh;
     line-height: 2em;
   }
   &.hidden {
     display: none;
   }
   &#hideShortcut {
-    translate: -7.5vw;
+    translate: -8vw;
     transition: all 0.5s ease-in;
   }
   &#showShortcut {
@@ -74,37 +30,6 @@ export const ParagraphOneSpacing = styled.p`
 export const SchemaContainer = styled.div`
   display: flex;
   justify-content: center;
-`;
-
-export const ShortcutChapter = styled.div`
-  padding: 0.5em;
-  border-radius: 10px;
-`;
-
-export const ShortcutContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  height: 200px;
-  border-radius: 0 0 10px 10px;
-  background: rgba(65, 65, 65);
-  &.hideShortcut {
-    opacity: 0;
-    translate: -20vw;
-    transition: all 0.5s ease-in-out;
-  }
-  &.showShortcut {
-    opacity: 1;
-    transition: all 0.5s ease-in-out;
-  }
-`;
-
-export const ShortcutAndLogoContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin-top: 8px;
-  position: sticky;
-  top: 5px;
-  height: min-content;
 `;
 
 export const TabContainer = styled.div`
