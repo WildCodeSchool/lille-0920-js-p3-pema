69c2ce4dc5dd88d92fe922a040f58d362e6d9887
AlexisCatrix <alexis.4x.ac@gmail.com>
2021-01-20 10:33:03 +0100

    Create a style file for App.js to set the display and background-color

---
+++ src/styled-components/AppStyled.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const AppContainer = styled.div`
+  display: flex;
+  margin: 3vw 5vw 3vw 1vw;
+  width: 95vw;
+`;
+
+export const BackgroundColor = styled.div`
+  display: flex;
+  align-items: center;
+  background: linear-gradient(180deg, white, black);
+  height: auto;
+  padding-bottom: 5vh;
+`;
