5ec0b1958ec53243c2dbabd68820b4139643a17f
Sarahshr <saraah.sahraoui@gmail.com>
2021-01-21 10:32:10 +0100

    Change structure of files and images

--- src/assets/clients.js
+++ src/assets/clients.js
@@ -1,18 +1,18 @@
-import sophartex from "./images/sophartex.jpeg";
-import unither from "./images/unither.png";
-import legendre from "./images/legendre.png";
-import alimakek from "./images/alimakek.png";
-import locapal from "./images/locapal.gif";
-import elivia from "./images/elivia.png";
-import dekra from "./images/dekra.png";
-import nes from "./images/nes.png";
-import cif from "./images/cif.png";
-import lagglo from "./images/lagglo.jpeg";
-import csgroup from "./images/cs-group.png";
-import cacom from "./images/cacom.png";
-import peg from "./images/peg.jpeg";
-import gaultfremont from "./images/gault-fremont.png";
-import clariant from "./images/clariant.png";
+import alimakek from "./images/clients/alimakek.png";
+import cacom from "./images/clients/cacom.png";
+import cif from "./images/clients/cif.png";
+import clariant from "./images/clients/clariant.png";
+import csgroup from "./images/clients/cs-group.png";
+import dekra from "./images/clients/dekra.png";
+import elivia from "./images/clients/elivia.png";
+import gaultfremont from "./images/clients/gault-fremont.png";
+import lagglo from "./images/clients/lagglo.jpeg";
+import legendre from "./images/clients/legendre.png";
+import locapal from "./images/clients/locapal.gif";
+import nes from "./images/clients/nes.png";
+import peg from "./images/clients/peg.jpeg";
+import sophartex from "./images/clients/sophartex.jpeg";
+import unither from "./images/clients/unither.png";
 
 export const myClients = [
   {
