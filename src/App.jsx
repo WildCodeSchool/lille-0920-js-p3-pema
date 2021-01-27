952f7f4f5669790d39e4a5c5b4a9da754e54988a
AlexisCatrix <alexis.4x.ac@gmail.com>
2021-01-27 13:54:10 +0100

    Import HeaderBar component

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,19 @@ import Chapters from "./Components/Chapters/Chapters";
 import Testimonies from "./Components/Testimonies/Testimonies";
 import Partners from "./Components/Partners/Partners";
 import Footer from "./Components/Footer/Footer";
+import HeaderBar from "./Components/Header/HeaderBar";
 
 export default function App() {
   return (
-    <div className="app">
+    <>
       <Reset />
+      <HeaderBar />
       <Header />
       <Timeline />
       <Chapters />
       <Testimonies />
       <Partners />
       <Footer />
-    </div>
+    </>
   );
 }
